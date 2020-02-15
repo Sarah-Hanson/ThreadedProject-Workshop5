5c81b54a6bd01d361e701b31100881cb86a436bd
Sarah <sarah.hanson@edu.sait.ca>
2020-02-14 19:56:17 -0700

    Implemented DBO as a singleton and slightly modified the implementation of its methods to save lines,effort and clock cycles by making it declare a a db connection on creation instead of every time a method is called.

--- ThreadedProject-Workshop5/Models/DatabaseIO/DBO.cs
+++ ThreadedProject-Workshop5/Models/DatabaseIO/DBO.cs
@@ -8,16 +8,30 @@ using ThreadedProject_Workshop5.Models.DBEntities.Pure_Objects;
 /*
 * Author: Sarah
 * Handles composite database interactions by combining different sqlqueries with the SQLAdapter to create meaningful
-* composite objects out of the base DBtable objects.
+* composite objects out of the base DBtable objects. Set up with singleton scheme to save on multiple instantiations
 */
 namespace ThreadedProject_Workshop5.Models {
     public class DBO {
+        // Singleton handler and constructor
+        private DBO() {
+            db = new TravelExpertsDB();
+        }
+        private static DBO instance = null;
+        public static DBO Instance {
+            get {
+                if (instance == null) {
+                    instance = new DBO();
+                }
+                return instance;
+            }
+        }
+
+        private TravelExpertsDB db;
         /*
          * Gets all packages/products and puts them in TravelPackage objects
          */
         public bool GetConglomerate(out List<TravelPackage> outlist) {
             List<Packages> packages;
-            TravelExpertsDB db = new TravelExpertsDB();
             outlist = new List<TravelPackage>();
             
             bool success = SQLAdapter.SQLAdapter.GetFromDB<Packages>(out packages, db);
@@ -32,7 +46,6 @@ namespace ThreadedProject_Workshop5.Models {
          */
         public bool GetConglomerate(out List<TravelAgency> outlist) {
             List<Agencies> agencies;
-            TravelExpertsDB db = new TravelExpertsDB();
             outlist = new List<TravelAgency>();
             bool success = true;
 
@@ -49,7 +62,6 @@ namespace ThreadedProject_Workshop5.Models {
          */
         public bool GetConglomerate(out List<TravelCustomer> outlist) {
             List<Customers> customers;
-            TravelExpertsDB db = new TravelExpertsDB();
             outlist = new List<TravelCustomer>();
 
             bool success = SQLAdapter.SQLAdapter.GetFromDB<Customers>(out customers, db);
@@ -59,11 +71,11 @@ namespace ThreadedProject_Workshop5.Models {
             }
             return success;
         }
-        
+        /*
+         * Updates the given customer's core details on the DB
+         */
         public bool updateConglomerate(TravelCustomer c) {
-            TravelExpertsDB db = new TravelExpertsDB();
-            bool success = SQLAdapter.SQLAdapter.UpdateInDB<Customers>(c, db);
-            return success;
+            return SQLAdapter.SQLAdapter.UpdateInDB<Customers>(c, db);
         }
     }
 }
