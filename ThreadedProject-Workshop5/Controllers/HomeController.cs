9b3ce7d038a3445ca9e23b5aa7d668acf3a9bac3
Sarah <sarah.hanson@edu.sait.ca>
2020-02-23 17:21:00 -0700

    Small changes

--- ThreadedProject-Workshop5/Controllers/HomeController.cs
+++ ThreadedProject-Workshop5/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace ThreadedProject_Workshop5.Controllers {
             return View();
 
         }
-        
+
         public ActionResult Contact() { //by Brando, additions by Sarah
             ViewBag.Loggin = IsLoggedIn();
             SQLAdapter.SQLAdapter.GetFromDB<Agents>(out List<Agents> agents, new TravelExpertsDB());
@@ -41,24 +41,27 @@ namespace ThreadedProject_Workshop5.Controllers {
         public ActionResult Booking(int id) {
             ViewBag.Loggin = IsLoggedIn();
             TravelBooking b = new TravelBooking();
+            b.BookingDate = DateTime.Today;
             b.PackageId = id;
-            b.CustomerId = (int)Session["UserID"];
+
             return View(b);
         }
         public ActionResult Book(TravelBooking model) {
-            ViewBag.Loggin = IsLoggedIn();
-            dbo.AddtoDB(model);
-            return View("index",null);
+            if (IsLoggedIn()) {
+                ViewBag.Loggin = IsLoggedIn();
+                model.CustomerId = (int)Session["UserID"];
+                dbo.AddtoDB(model);
+            }
+            return View("index", null);
         }
-        public ActionResult Register() { 
+        public ActionResult Register() {
             //By Brandon 
             ViewBag.Loggin = IsLoggedIn();
             return View();
         }
-        public ActionResult MakeRegister(TravelCustomer model)
-        {//By Brandon with significant help from Sarah
+        public ActionResult MakeRegister(TravelCustomer model) {//By Brandon with significant help from Sarah
             SQLAdapter.SQLAdapter.InsertToDB<Customers>(model, new TravelExpertsDB());
-            
+
             dbo.GetConglomerate(out TravelCustomer cust, model.CustUserName);
             LogUserIn(model.CustUserName, model.CustomerID);
             ViewBag.Loggin = IsLoggedIn();
@@ -69,7 +72,7 @@ namespace ThreadedProject_Workshop5.Controllers {
          */
         public ActionResult UserProfile() {
             ViewBag.Loggin = IsLoggedIn();
-            if (debug) { LogUserIn("user1",104); }
+            if (debug) { LogUserIn("user1", 104); }
 
             dbo.GetConglomerate(out TravelCustomer travelCust, Session["UserLogin"].ToString());
             travelCust.CustPassword = SimpleSecurity.Decrypt(travelCust.CustPassword);
@@ -87,16 +90,16 @@ namespace ThreadedProject_Workshop5.Controllers {
         [HttpPost]
         public ActionResult Login(string UserName, string Password) {
             Password = SimpleSecurity.Encrypt(Password);
-            SQLAdapter.SQLAdapter.GetFromDB<Customers>(out List<Customers> custs,new TravelExpertsDB());
-            foreach(Customers tc in custs) {
-                if((tc.CustUserName.Equals(UserName)) &&
+            SQLAdapter.SQLAdapter.GetFromDB<Customers>(out List<Customers> custs, new TravelExpertsDB());
+            foreach (Customers tc in custs) {
+                if ((tc.CustUserName.Equals(UserName)) &&
                     tc.CustPassword.Equals(Password)) {
                     LogUserIn(UserName, tc.CustomerID); // Its a match!
                     break;
                 }
             }
             ViewBag.Loggin = IsLoggedIn();
-            return View("index",null);
+            return View("index", null);
         }
         public ActionResult Logout(string UserName, string Password) {
             LogUserOut();
